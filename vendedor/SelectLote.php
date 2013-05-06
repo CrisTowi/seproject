28d706b3875584461d0388eede45765b98f232c3
Shtefany <viridianacabrera@hotmail.com>
2013-05-06 01:27:06 -0500

    Arreglo

--- vendedor/SelectLote.php
+++ vendedor/SelectLote.php
@@ -2,7 +2,7 @@
 	include("../php/DataConnection.class.php");
 	include("../php/Validations.class.php");
 	$db = new DataConnection();
-	$result = $db->executeQuery("SELECT idlote FROM lote where Estado is NULL and cantidadProducto > 0 and idProducto=".$_GET['id']." order by Fecha_Cad");
+	$result = $db->executeQuery("SELECT idlote FROM lote where Estado is NULL and cantidadProducto > 0 and idProducto=".$_GET['id']." order by Fecha_Caducidad");
 	$totalFilas    =    mysql_num_rows($result);
 	if($totalFilas>0){
 	while( $dato = mysql_fetch_assoc($result) ){
