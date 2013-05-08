8411b1c4f8a2daa915ec8339cd1bc7209f20ec19
Juan Carlos <juniocarl@gmail.com>
2013-05-08 18:47:26 -0500

    Correccion a validaciones en el JS

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,18 +1,23 @@
 function validarCurp(curp){
 	curp=curp.toUpperCase().trim();
-	var re = /[A-Z]{4}[0-9]{6}[H,M][A-Z]{5}[A-Z0-9]{1}[0-9]{1}/;
+	var re = /^[A-Z]{4}[0-9]{6}[H,M][A-Z]{5}[A-Z0-9]{1}[0-9]{1}$/;
 	return re.exec(curp);
 }
 function validarRFC(rfc){
 	rfc=rfc.toUpperCase().trim();
-	if(rfc.length<=12)
-	{var re = /[A-Z]{3}[0-9]{6}[A-Z0-9]{3}/;}
+	var re;
+	if(rfc.length<=12){
+	{
+		re = /^[A-Z]{3}[0-9]{6}[A-Z0-9]{3}$/;
+	}
 	else 
-	{var re = /[A-Z]{4}[0-9]{6}[A-Z0-9]{3}/;}
+	{
+		re = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
+	}
 	return re.exec(rfc);
 }
 function validarTel(tel){
-	var re = /[0-9]{2}[-. ]?[0-9]{2}[-. ]?[0-9]{2}[-. ]?[0-9]{2}/;
+	var re = /^[0-9]{2}[-. ]?[0-9]{2}[-. ]?[0-9]{2}[-. ]?[0-9]{2}$/;
 	return re.exec(tel);
 }
 function validarEma(ema) {
