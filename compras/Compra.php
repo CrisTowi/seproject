9915dabe4b36c1cf0edf590a4135b63fc86e1329
Carlos Jiménez <carlos_jimenez_347@hotmail.com>
2013-05-14 00:36:38 -0500

    Se quita el bug de gestion de materia prima.
    Funcionalidad de modulo de compras completa. Falta trigger para
    proveedores.

--- compras/Compra.php
+++ compras/Compra.php
@@ -25,32 +25,24 @@
                 <form id="altaCompra" action="AgregaCom.php" name="altaCompra" method ="POST">
 				<div id="content">
 					<div class="box">
-						Proveedor: <?php include("SelectProveedores.php"); ?>
-						<table id="productos">
-						<tr>
-								<td>
-									Producto:<br>
-									<select id='sproductos1'>
-										<div >
-											<?php include("SelectProductos.php"); ?>
-										</div>
+						<table>
+							<tr>
+								<td>Proveedor: <?php include("SelectProveedores.php"); ?></td>
+							</tr>
+							<tr>
+								<td>Producto:
+									<select id='selectProductos'>
+										<?php include("SelectProductos.php"); ?>
 									</select>
-
+									<img src="../img/add.png" onClick="agregarProducto(document.altaCompra.selectProductos.value)" class='clickable'/>
 								</td>
-								<td>
-									Cantidad:<br> <input id="cantidad1" type="number" onblur="valida(this.value,'cantidad','numero');" class="cantidad" value="0" min="0" max="10000" >
-									<span id="cantidad"></span>
-								
-								</td>
-									
-								<td>
-									Precio Unitario:$<br> <input type="number" class="cantidad" onblur="valida(this.value,'unitario','numero');" onChange="calcular_total();"  value="0" min="0" max="1000000"> 
-									<span id="unitario"></span>
-								</td>
-							   <td>
-									<img src="../img/add.png" onClick="agregarProducto()" class='clickable'/>
-							   </td>
-						</tr>
+							</tr>
+						</table>
+						
+						<table id="productos">
+							<tr id="msgTable" class="tr-cont">
+								<td colspan="2">Aun no se han agregado productos</td>
+							</tr>
 						</table>
 							Total: 
 							<input readonly type="text" id="total" name="total"> 
@@ -76,16 +68,20 @@
 <script type="text/javascript">
 
 function agregarCompra(){
-		if( document.getElementById("total").value!= 0 && document.altaCompra.proveedor[document.altaCompra.proveedor.selectedIndex].value!=-1 && document.altaCompra.sproductos1[document.altaCompra.sproductos1.selectedIndex].value!=-1 ){
-		var filas = document.getElementById("productos").rows.length;
+		if( document.getElementById("total").value!= 0 && document.altaCompra.proveedor[document.altaCompra.proveedor.selectedIndex].value!=-1 && document.altaCompra.selectProductos[document.altaCompra.selectProductos.selectedIndex].value!=-1 ){
+		var filas = document.getElementById("productos").rows.length - 1;	//Menos 1 por el encabezado
 		//alert(filas);
 		parametros = "numprod="+ filas + "&"  ;
 		parametros += "proveedor=" + document.getElementById('proveedor').value+"&";
+		
+		producto = document.getElementsByClassName("idProducto");	//Recuperamos los input con las cantidades
+		cantidades = document.getElementsByClassName("cantidad");		//Recuperamos los input con los precios
+		
 		for (var i =1 ;i<=filas ;i++ ){
-			parametros+= "producto"+ i+"=" + document.getElementById('sproductos'+i).value + "&";
+			parametros+= "producto"+ i+"=" + producto[i-1].value + "&";
 		}
 		for (var j =1 ;j<=filas ;j++ ){
-			parametros+= "cantidad"+ j+"=" + document.getElementById('cantidad'+j).value + "&";
+			parametros+= "cantidad"+ j+"=" + cantidades[j-1].value + "&";
 		}
 		
 		parametros += "total=" + document.getElementById('total').value;
@@ -121,7 +117,7 @@ function valida( str, target, validate ){
 				var re = /^\d+\.?\d*$/;
 				ok = re.exec(str);
 				if ( !ok ){
-					document.getElementById(target).innerHTML = "<img src='../img/error.png' />";	
+					document.getElementById(target).innerHTML = "<img src='../img/error.png' />";
 				}else{
 					document.getElementById(target).innerHTML = "<img src='../img/ok.png' />";
 				}
@@ -129,91 +125,127 @@ function valida( str, target, validate ){
 		}
 	}
 
-function Contar(){
-	var numero = document.altaCompra.sproductos1;
-	return numero.length;
-
+function loadTable()
+{
+	//Se llena el select de productos
+	filtro = document.getElementById('proveedor').value;
+	sendPetitionSync("SelectProductos.php?name=" + filtro , "selectProductos",document);
+	
+	//Se borra la tabla
+	table = document.getElementById('productos');
+	while(table.rows.length > 0)
+		table.deleteRow(0);
+	
+	//Se pone el msg
+	nuevaFila = document.getElementById('productos').insertRow(-1);
+	nuevaFila.className = "tr-cont";
+	nuevaFila.id = "msgTable";
+	nuevaCelda = nuevaFila.insertCell(-1);
+	nuevaCelda.innerHTML = "Aun no se han agregado productos";
+	
+	//Total = 0
+	$("#total").val(0);
 }
-
-
-function loadTable(){
-		var  tabla=Contar()-1;
-		filtro = document.getElementById('proveedor').value;
-		
-		while(tabla>=0){
-				var aux = tabla+1;
-				var aux2 = "sproductos"+aux;
-				sendPetitionSync("SelectProductos.php?name=" + filtro ,aux2,document);
-				tabla--;	
-		}
-	}
 
 	function ValidaSoloNumeros() {
  if ((event.keyCode < 48) || (event.keyCode > 57))
   event.returnValue = false;
 }
 	
-	function calcular_total() {
-	importe_total = 0
-	subtotal=0
-	aux = 0
-	$(".cantidad").each(
-		function(index, valor) {
-			if(index==0 || (index%2)==0){
-			aux =valor.value;
-			}else if((index%2)!=0){
-				subtotal = aux * eval($(this).val());
-				importe_total+=subtotal;
-			}
-		}
-	);
-	$("#total").val(importe_total);
-}
+	function calcular_total() 
+	{
+		importe_total = 0
+		cantidades = document.getElementsByClassName("cantidad");	//Recuperamos los input con las cantidades
+		precios = document.getElementsByClassName("precio");		//Recuperamos los input con los precios
+		for(i = 0; i < cantidades.length; i++)
+			importe_total = importe_total + (cantidades[i].value * precios[i].value); 
 	
-var posicionCampo=2;
+		$("#total").val(importe_total);
+	}
+	
+	var idProducto = 1;
 
-function disminuir(){
-	posicionCampo--;
-}
-function agregarProducto(){
-		var opciones= posicionCampo;
-		var productos= Contar();
-		if (productos-1>opciones-1){
+	function agregarProducto(id)
+	{		
+		filas = document.getElementById('productos').rows.length - 1;	//menos 1 por la fila del mensaje inicial
+			
+		//Si no hay nada creamos los header
+		if(filas == 0)
+		{
+			//Quitamos el msj inicial
+			table = document.getElementById('productos');
+			table.deleteRow(0);
+			
+			//Agregamos los header
 			nuevaFila = document.getElementById('productos').insertRow(-1);
-			nuevaFila.id=posicionCampo;
-			nuevaCelda=nuevaFila.insertCell(-1);
-			nuevaCelda.innerHTML="<select id='sproductos"+posicionCampo+"'><?php include("SelectProductos.php"); ?></select>";
-			nuevaCelda=nuevaFila.insertCell(-1);
-			nuevaCelda.innerHTML="<td><input type='text' id='cantidad"+posicionCampo+"' onblur=\"valida(this.value,'cantidades"+posicionCampo+"','numero\');\" class='cantidad' value='0'><span id='cantidades"+posicionCampo+"'></span></td>";
-			nuevaCelda=nuevaFila.insertCell(-1);
-			nuevaCelda.innerHTML="<td><input type='text' id='precio"+posicionCampo+"' class='cantidad' onChange='calcular_total();' onBlur=\"valida(this.value,'unitario"+posicionCampo+"','numero');\" value='0'><span id='unitario"+posicionCampo+"'></span></td>";
-			nuevaCelda=nuevaFila.insertCell(-1);
-			nuevaCelda.innerHTML="<td><img src= '../img/less.png'  class='clickable' onclick='eliminarUsuario(this);calcular_total();disminuir();'></td>";
-			posicionCampo++;
-			loadTable();
+			nuevaFila.id = "header";
+			nuevaFila.className = "tr-header";
+			nuevaFila.style.backgroundColor = "#FFA154";
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML = "Producto";
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML = "Cantidad";
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML = "Precio";
 		}
-}
+			
+		if (!estaAgregado(id))	//Si no se ha agregado el producto
+		{
+			indice = document.altaCompra.selectProductos.selectedIndex;
+			idMateriaPrima = document.altaCompra.selectProductos.value;	//Es el valor del option
+			texto = document.altaCompra.selectProductos.options[indice].text;	//Es lo que ve el usuario
+				
+			nuevaFila = document.getElementById('productos').insertRow(-1);
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML = "<input type='hidden' name='idProducto"+idProducto+"' class='idProducto' value='"+idMateriaPrima+"' />" + texto;
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML="<input type='text' id='cantidad"+idProducto+"' class='cantidad' onChange='calcular_total();' onblur=\"valida(this.value,'cantidades"+idProducto+"','numero');\" value='0'><span id='cantidades"+idProducto+"'></span>";
+			nuevaCelda=nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML="<input type='text' id='precio"+idProducto+"' class='precio' onChange='calcular_total();' onBlur=\"valida(this.value,'unitario"+idProducto+"','numero');\" value='0'><span id='unitario"+idProducto+"'></span>";		
+			nuevaCelda=nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML="<img src= '../img/less.png' class='clickable' onclick='eliminarProducto(this);'>";
+			idProducto++;
+		}
+		else
+			alert("El producto ya se ha agregado");
+	}
 
-/* Definimos la función eliminarUsuario, la cual se encargará de quitar la fila completa del formulario. No es necesario hacer modificaciones sobre este código */
-
-function eliminarUsuario(obj){
-
-var oTr = obj;
-
-while(oTr.nodeName.toLowerCase()!='tr'){
-
-oTr=oTr.parentNode;
-
-}
-
-var root = oTr.parentNode;
-
-root.removeChild(oTr);
-
-}
-
-/* Cerramos el código Javascript */
+	//Funcion para saber si ya se agrego a la tabla el producto
+	function estaAgregado(id)
+	{
+		arrayId = document.getElementsByClassName("idProducto");
+		for(i = 0; i < arrayId.length; i++)
+		{
+			if(arrayId[i].value == id)
+				return true;
+		}
+		return false;
+	}
 	
+	function eliminarProducto(obj)
+	{
+		var oTr = obj;
+		while(oTr.nodeName.toLowerCase()!='tr'){
+			oTr=oTr.parentNode;
+		}
+		var root = oTr.parentNode;
+		root.removeChild(oTr);
+		
+		filas = document.getElementById('productos').rows.length - 1;
+		
+		//Si se borraron todos los produtos hay que poner de nuevo el msg inicial
+		if(filas == 0)
+		{
+			table = document.getElementById('productos');
+			table.deleteRow(0);
+			
+			nuevaFila = document.getElementById('productos').insertRow(-1);
+			nuevaFila.className = "tr-cont";
+			nuevaFila.id = "msgTable";
+			nuevaCelda = nuevaFila.insertCell(-1);
+			nuevaCelda.innerHTML = "Aun no se han agregado productos";
+		}
+	}
 	
 	
 </script>
