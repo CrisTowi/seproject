97637909b3f3f443bb751f0167a1e58ff4ff8564
Juan Carlos <juniocarl@gmail.com>
2013-05-22 01:09:16 -0500

    Correccion a inventarios en Agregar MP
    Se eliminan variables sin uso en AgregarMA.php

--- inventarios/AgregarMA.php
+++ inventarios/AgregarMA.php
@@ -7,15 +7,11 @@
 	$idm       =	Validations::cleanString($_GET['idm']);
 	$idp       =	Validations::cleanString($_GET['idp']);
 	$cantidad  = 	Validations::cleanString($_GET['cantidad']);
-	$unidad    = 	Validations::cleanString($_GET['unidad']);
-	$precio    = 	Validations::cleanString($_GET['precio']);
 	$fecha_l   =	Validations::cleanString($_GET['fecha_l']);
 	$fecha_c   =	Validations::cleanString($_GET['fecha_c']);
 
-	/* Decodifica los caracteres de la URL */
-	$direccion = str_replace("%23", "#", $direccion);
+	$precio = 0;
 	
-
 	//echo  $idc.$nombre.$proveedor.$cantidad.$unidad.$precio.$fecha_c.$fecha_l;
 	if (is_numeric($cantidad)){
 
